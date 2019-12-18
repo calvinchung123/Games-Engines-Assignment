4d7467d57353826780ae2baa3ab433d21556f19c
Calvin Chung <c15529113@mydit.ie>
2019-12-18 23:00:58 +0000

    commit

---
+++ Games Engines Assignment/Assets/_Scripts/BallPulse.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallPulse : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    public Color startColor;
+    public Color endColor;
+    public bool repeatable = false;
+    float startTime;
+
+    void Start()
+    {
+        startTime =Time.time;
+    }
+    void Update()
+    {
+        if(!repeatable)
+        {
+            float t = (Time.time - startTime) * speed;
+            GetComponent<Renderer>().material.color = Color.Lerp(startColor, endColor, t);
+        }
+        else
+        {
+            float t = (Mathf.Sin(Time.time - startTime) * speed);
+            GetComponent<Renderer>().material.color = Color.Lerp(startColor, endColor, t);
+        }
+    }
+}
+
